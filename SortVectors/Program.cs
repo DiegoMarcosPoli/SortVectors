a139731ec51ca07be683563e2d7b338a661ce5ad
Diego Marcos Poli <DIEGO MARCOS.POLI@local.ittcesena.it>
2020-10-07 11:47:48 +0200

    second step ordinamento

--- SortVectors/Program.cs
+++ SortVectors/Program.cs
@@ -13,8 +13,18 @@ namespace SortVectors
                 vett[i] = r.Next(-20, 25);
                 Console.WriteLine(vett[i]);                
             }
+            Ordina(vett);
+            for (int i = 0; i < vett.Length; i++)
+            {
+                Console.WriteLine(vett[i]);
+            }
             Console.ReadKey();
+
         }
+    }
+    public static void Ordina(int[] v)
+    {
+        Array.Sort(v);
     }
 
 }
