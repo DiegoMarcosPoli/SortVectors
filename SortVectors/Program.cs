b096c71097e729cfde34addb4fd15617da412b4e
Diego Marcos Poli <DIEGO MARCOS.POLI@local.ittcesena.it>
2020-10-07 11:06:28 +0200

    first step

---
+++ SortVectors/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SortVectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vett = new int[10];
+            Random r = new Random();
+            for (int i = 0; i < vett.Length; i++)
+            {
+                vett[i] = r.Next(-20, 25);
+                Console.WriteLine(vett[i]);                
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
