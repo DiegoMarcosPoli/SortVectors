ffeb15b82978d526d8e59c5780f1936d7879948b
diego marcos.poli <diego marcos.poli@PC7317>
2020-10-21 11:21:06 +0200

    Metodo calcolo min inserito

--- SortVectors/Program.cs
+++ SortVectors/Program.cs
@@ -13,19 +13,27 @@ namespace SortVectors
                 vett[i] = r.Next(-20, 25);
                 Console.WriteLine(vett[i]);                
             }
+            Console.WriteLine();
+            Console.ReadKey();
             Ordina(vett);
+            int max = Massimo(vett);
             for (int i = 0; i < vett.Length; i++)
             {
                 Console.WriteLine(vett[i]);
             }
+            Console.WriteLine();
             Console.ReadKey();
-
+            Console.WriteLine("il valore massimo è " + max);
+            
+            Console.ReadKey();
         }
         public static void Ordina(int[] v)
         {
             Array.Sort(v);
         }
+        public static int Massimo(int[] v)
+        {
+            return v[9];
+        }
     }
-   
-
 }
