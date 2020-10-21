da374c943c333bcb0d3e0bd697062791c5715a22
cristofer.napolitano <cristofer.napolitano@PC7315>
2020-10-21 11:24:46 +0200

    ho inserito il metodo per il calcolo del minimo e spostato il metodo Ordina dentro la classe Program

--- SortVectors/Program.cs
+++ SortVectors/Program.cs
@@ -8,23 +8,35 @@ namespace SortVectors
         {
             int[] vett = new int[10];
             Random r = new Random();
+
+
             for (int i = 0; i < vett.Length; i++)
             {
                 vett[i] = r.Next(-20, 25);
                 Console.WriteLine(vett[i]);                
             }
+
             Ordina(vett);
+
             for (int i = 0; i < vett.Length; i++)
-            {
                 Console.WriteLine(vett[i]);
-            }
+
+            Console.WriteLine($"il valore minimo è {Minimo(vett)}");
+
             Console.ReadKey();
 
         }
+
+        public static void Ordina(int[] v)
+        {
+            Array.Sort(v);
+        }
+
+        static int Minimo(int[] v)
+        {
+            return v[0];
+        }
     }
-    public static void Ordina(int[] v)
-    {
-        Array.Sort(v);
-    }
+    
 
 }
